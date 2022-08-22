88c2db4a52e0c2f05ef303b52df7b19b8a2736cf
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-08-23 05:23:44 +0530

    Initial Commit : Create directory structure and setup express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`error in starting the server : ${err}`);
+        return;
+    }
+
+    console.log(`server is up and running on port : ${port}`);
+});
