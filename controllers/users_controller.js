6f62e58371339a7041a935feedbbcc49820be447
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-08-23 16:19:15 +0530

    create signin and signup page

---
+++ controllers/users_controller.js
@@ -0,0 +1,11 @@
+module.exports.signIn = function(req, res){
+    return res.render('sign_in', {
+        title: 'Sign In'
+    });
+}
+
+module.exports.signUp = function(req, res){
+    return res.render('sign_up', {
+        title: 'Sign Up'
+    });
+}
