5d86a7ec3c6b8959867e7a482c4e83a68b75bf64
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-08-25 14:40:34 +0530

    created schema for issue and updated project schema

---
+++ models/issue.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const issueSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    project: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Project'
+    }
+},{
+    timestamps: true
+});
+
+const Issue = mongoose.model('Issue', issueSchema);
+
+module.exports = Issue;
