7182363c18be2eab4a99f919e0542752d4e7aa33
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-08-31 00:42:34 +0530

    all routes made inaccessible when user is not logged in except user/sign-in and user/sign-up

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const passport = require('passport');
 const homeController = require('../controllers/home_controller');
 
-router.get('/', passport.checkAuthentication, homeController.home);
 router.use('/users', require('./users'));
+
+// put every route behind authentication, only user/sign-in, user/sign-up routes are accessible when not signed in
+router.use(passport.checkAuthentication);
+
+router.get('/', homeController.home);
 router.use('/project', require('./project'));
 
 module.exports = router;
