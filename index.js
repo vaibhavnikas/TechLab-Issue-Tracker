dcec0e4f35d141547efcc72bb97cc536a7ae3383
Vaibhav Nikas <march4vaibhav@gmail.com>
2022-09-01 01:41:24 +0530

    setup gulp for css and js minification

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
 const flash = require('connect-flash');
 const customMware = require('./config/middleware');
+const path = require('path');
 
 app.use(sassMiddleware({
     src: './assets/scss',
